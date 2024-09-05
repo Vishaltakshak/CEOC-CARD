00726fa4a2e9f0659737a13dee37c837e0807907
Vishaltakshak <127527299+Vishaltakshak@users.noreply.github.com>
2024-09-06 00:35:08 +0530

    overflow problem solved

--- src/Components/MemberBenefits.js
+++ src/Components/MemberBenefits.js
@@ -7,11 +7,11 @@ import "../css/testimonial.css";
 const MemberBenefits = () => {
 	return (
 		<>
-			<div className="container-fluid benefits-block-parent">
+			<div className="container-fluid benefits-block-parent benefits-block-parent-white">
 				<div className="container pe-0 ps-0">
 					<HomePageHeading pagetopic="OUR NEWEST MEMBER BENEFITS" pagetopiclink="View all new benefits"></HomePageHeading>
 					{/* <div className="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 member-benefit-block-parent scroll-container"> */}
-					<div className="member-benefit-block-parent scroll-container testimonial-container">
+					<div className="flex testimonial-container">
 						<div className="flex scroll-inner-div-6-benefits test-slider">
 							<MemberBenefitsList></MemberBenefitsList>
 							<MemberBenefitsList></MemberBenefitsList>
